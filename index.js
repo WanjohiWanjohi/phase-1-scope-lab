4c4c98ab741ccd00b8ffbd0ea676e002217a6845
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-06-10 22:03:16 +0300

    commit tree a869caa6d33e52f08e240953e250f129af251eb9

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your solution in this file!
+var customerName ="bob" ;
+const leastFavoriteCustomer  = "ann"
+function upperCaseCustomerName() { customerName= customerName.toUpperCase();}
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer ="jay";
+}
